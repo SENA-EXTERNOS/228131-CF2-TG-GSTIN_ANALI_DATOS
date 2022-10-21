da0d8602e9ed3d80973d45ba4fd8fe8ab772806b
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-10-20 20:47:48 -0500

    Tema 2
    Se empieza tema 2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,7 +5,18 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 Tipos de variables
+  
+    .p-3.mb-5
+      .row.justify-content-around.align-items-center
+        .col-3.col-sm-2.col-lg-2
+          img(src='@/assets/curso/temas/T2/img-1.svg')
+        .col
+          .row.justify-content-between.align-items-center
+            .col.mb-3.mb-sm-0
+              p.mb-3 En este apartado se abordan los diferentes tipos de variables: cualitativa o categóricas, cuantitativas, entre las cuantitativas o numéricas las discretas y las continuas. Es importante seguir explorando el contenido para fortalecer el aprendizaje en los tipos de variables.
+              p A continuación, se explican los diferentes tipos de variables mediante la teoría y la práctica, a través un ejemplo real, con gráficos generados dentro de <i>Google Colab</i>, utilizando el lenguaje <i>Python</i>. 
+
 
 </template>
 
